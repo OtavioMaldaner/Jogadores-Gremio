ed12040402caff7986bbf1b75a7fa7c6498be561
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-06-01 14:52:21 -0300

    Jogadores.ts

--- jogadores-gremio/src/components/Body/index.tsx
+++ jogadores-gremio/src/components/Body/index.tsx
@@ -1,137 +1,7 @@
 import './style.css'
 import { useState } from 'react';
+import { Jogadores } from '../../jogadores/Jogadores';
 const Body = () => {
-
-    const jogadores = [{
-        name: "Gabriel Grando",
-        position: "goleiro"
-    }, {
-        name: "Brenno",
-        position: "goleiro"
-    }, {
-        name: "Phelipe Megiolaro",
-        position: "goleiro"
-    }, {
-        name: "Adriel",
-        position: "goleiro"
-    }, {
-        name: "Kannemann",
-        position: "zagueiro"
-    }, {
-        name: "Geromel",
-        position: "zagueiro"
-    }, {
-        name: "Bruno Alves",
-        position: "zagueiro"
-    }, {
-        name: "Diogo Barbosa",
-        position: "lateral esquerdo"
-    }, {
-        name: "Rodrigues",
-        position: "zagueiro"
-    }, {
-        name: "Nícolas",
-        position: "lateral esquerdo"
-    }, {
-        name: "Léo Gomes",
-        position: "lateral direito"
-    }, {
-        name: "Edílson",
-        position: "lateral direito"
-    }, {
-        name: "Rodrigo Ferreira",
-        position: "lateral direito"
-    }, {
-        name: "Heitor",
-        position: "zagueiro"
-    }, {
-        name: "Gustavo Marins",
-        position: "zagueiro"
-    }, {
-        name: "Natã",
-        position: "zagueiro"
-    }, {
-        name: "Lucas Kawan",
-        position: "lateral direito"
-    }, {
-        name: "Cuiabano",
-        position: "lateral esquerdo"
-    }, {
-        name: "Thiago Rosa",
-        position: "lateral esquerdo"
-    }, {
-        name: "Ferreira",
-        position: "atacante"
-    }, {
-        name: "Benítez",
-        position: "meia"
-    }, {
-        name: "Jaminton Campaz",
-        position: "meia"
-    }, {
-        name: "Lucas Silva",
-        position: "volante"
-    }, {
-        name: "Mathias Villasanti",
-        position: "volante"
-    }, {
-        name: "Janderson",
-        position: "atacante"
-    }, {
-        name: "Thiago Santos",
-        position: "volante"
-    }, {
-        name: "Pedro Lucas",
-        position: "meia"
-    }, {
-        name: "Biel",
-        position: "meia"
-    }, {
-        name: "Victor Bobsin",
-        position: "volante"
-    }, {
-        name: "Bitello",
-        position: "meia"
-    }, {
-        name: "Fernando Henrique",
-        position: "volante"
-    }, {
-        name: "Michel",
-        position: "volante"
-    }, {
-        name: "Matheus Frizzo",
-        position: "volante"
-    }, {
-        name: "Matheus Sarará",
-        position: "volante"
-    }, {
-        name: "Gabriel Silva",
-        position: "meia"
-    }, {
-        name: "Ronald",
-        position: "volante"
-    }, {
-        name: "Jhonata Varela",
-        position: "volante"
-    }, {
-        name: "Jhonata Robert",
-        position: "atacante"
-    }, {
-        name: "Diego Souza",
-        position: "atacante"
-    }, {
-        name: "Elkeson",
-        position: "atacante"
-    }, {
-        name: "Ricardinho",
-        position: "atacante"
-    }, {
-        name: "Elias Manoel",
-        position: "atacante"
-    }, {
-        name: "Kevin",
-        position: "atacante"
-    }];
     type Props = {
         number: string;
         cvc: string;
@@ -177,8 +47,8 @@ const Body = () => {
         let sum = sumCreditCard + date;
         let divide = sum / Number(creditCard.cvc);
         let multiply = divide * 1000;
-        let rest = Math.round(multiply % jogadores.length);
-        alert(`Você é o ${jogadores[rest].position} ${jogadores[rest].name}`);
+        let rest = Math.round(multiply % Jogadores.length);
+        alert(`Você é o ${Jogadores[rest].position} ${Jogadores[rest].name}`);
     }
     return(
         <div className="container">
---
+++ jogadores-gremio/src/jogadores/Jogadores.ts
@@ -0,0 +1,134 @@
+export type Jogador = {
+    name: string;
+    position: string;
+}
+export const Jogadores = [{
+    name: "Gabriel Grando",
+    position: "goleiro"
+}, {
+    name: "Brenno",
+    position: "goleiro"
+}, {
+    name: "Phelipe Megiolaro",
+    position: "goleiro"
+}, {
+    name: "Adriel",
+    position: "goleiro"
+}, {
+    name: "Kannemann",
+    position: "zagueiro"
+}, {
+    name: "Geromel",
+    position: "zagueiro"
+}, {
+    name: "Bruno Alves",
+    position: "zagueiro"
+}, {
+    name: "Diogo Barbosa",
+    position: "lateral esquerdo"
+}, {
+    name: "Rodrigues",
+    position: "zagueiro"
+}, {
+    name: "Nícolas",
+    position: "lateral esquerdo"
+}, {
+    name: "Léo Gomes",
+    position: "lateral direito"
+}, {
+    name: "Edílson",
+    position: "lateral direito"
+}, {
+    name: "Rodrigo Ferreira",
+    position: "lateral direito"
+}, {
+    name: "Heitor",
+    position: "zagueiro"
+}, {
+    name: "Gustavo Marins",
+    position: "zagueiro"
+}, {
+    name: "Natã",
+    position: "zagueiro"
+}, {
+    name: "Lucas Kawan",
+    position: "lateral direito"
+}, {
+    name: "Cuiabano",
+    position: "lateral esquerdo"
+}, {
+    name: "Thiago Rosa",
+    position: "lateral esquerdo"
+}, {
+    name: "Ferreira",
+    position: "atacante"
+}, {
+    name: "Benítez",
+    position: "meia"
+}, {
+    name: "Jaminton Campaz",
+    position: "meia"
+}, {
+    name: "Lucas Silva",
+    position: "volante"
+}, {
+    name: "Mathias Villasanti",
+    position: "volante"
+}, {
+    name: "Janderson",
+    position: "atacante"
+}, {
+    name: "Thiago Santos",
+    position: "volante"
+}, {
+    name: "Pedro Lucas",
+    position: "meia"
+}, {
+    name: "Biel",
+    position: "meia"
+}, {
+    name: "Victor Bobsin",
+    position: "volante"
+}, {
+    name: "Bitello",
+    position: "meia"
+}, {
+    name: "Fernando Henrique",
+    position: "volante"
+}, {
+    name: "Michel",
+    position: "volante"
+}, {
+    name: "Matheus Frizzo",
+    position: "volante"
+}, {
+    name: "Matheus Sarará",
+    position: "volante"
+}, {
+    name: "Gabriel Silva",
+    position: "meia"
+}, {
+    name: "Ronald",
+    position: "volante"
+}, {
+    name: "Jhonata Varela",
+    position: "volante"
+}, {
+    name: "Jhonata Robert",
+    position: "atacante"
+}, {
+    name: "Diego Souza",
+    position: "atacante"
+}, {
+    name: "Elkeson",
+    position: "atacante"
+}, {
+    name: "Ricardinho",
+    position: "atacante"
+}, {
+    name: "Elias Manoel",
+    position: "atacante"
+}, {
+    name: "Kevin",
+    position: "atacante"
+}]
